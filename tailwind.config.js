22510efeccca5220be4a890e7ccd145ed0a13eae
Nomenclaitor <qiyao43@gmail.com>
2024-04-08 18:30:00 +0100

    Landing page V1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,49 @@
 /** @type {import('tailwindcss').Config} */
+const {log} = require("node:util");
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio')
+  ],
+}
+
+let lista = {
+  obj1: {
+    'title': 'titulo del producto1',
+    'description': 'descripcion del producto 1',
+    'price': 'precio p1',
+    'id': 'id prod1'
+  },
+  obj2: {
+    'title': 'titulo del producto1',
+    'description': 'descripcion del producto 1',
+    'price': 'precio p1',
+    'id': 'id prod1'
+  },
+  obj3: {
+    'title': 'titulo del producto1',
+    'description': 'descripcion del producto 1',
+    'price': 'precio p1',
+    'id': 'id prod1'
+  },
+  user: {
+    'nombre': 'nombre',
+    'password':'password',
+    'birthday': 'birthday'
+  }
+}
+
+async function getProducts() {
+  let data = await fetch(`${window.location}/get-productos`)
+    .then(data => {
+      return JSON.parse(data);
+    })
+}
+
+for (const product in lista) {
+  log()
 }
 
